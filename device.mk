9a36a1793620bd53397336bd72ef92d1928ba05d
wodanesdag <wodanesdag@proton.me>
2023-10-13 02:03:39 +0700

    begonia: Add GMS packages

--- device.mk
+++ device.mk
@@ -115,6 +115,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
+# GMS
+include vendor/gms/gms.mk
+
 # DT2W
 PRODUCT_PACKAGES += \
     DT2W-Service-Begonia
