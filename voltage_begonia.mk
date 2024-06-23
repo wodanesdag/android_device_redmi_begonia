0520c7d6c4d6b9552f9b8f3f042c6806630436fc
Saikrishna1504 <saikrishna26918@gmail.com>
2024-06-23 09:27:29 +0530

    begonia: update build type
    * bye bye official
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- voltage_begonia.mk
+++ voltage_begonia.mk
@@ -40,7 +40,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Official-ify
-VOLTAGE_BUILD_TYPE := OFFICIAL
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
