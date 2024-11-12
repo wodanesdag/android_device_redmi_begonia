329e6a0b6f87abb4fd79cb4edab63f969498bc26
Michael Bestas <mkbestas@lineageos.org>
2024-11-12 03:09:15 +0000

    begonia: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2021 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -55,67 +55,86 @@ fi
 function blob_fixup {
     case "${1}" in
         lib/libsink.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.lights-service.mediatek)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.light-V1-ndk_platform.so" "android.hardware.light-V1-ndk.so" "${2}"
             ;;
         vendor/lib64/libudf.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "${2}"
             ;;
         vendor/lib64/libmtkcam_stdutils.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
-        vendor/lib/hw/audio.primary.mt6785.so)
-            ;&
+        vendor/lib/hw/audio.primary.mt6785.so|\
         vendor/lib64/hw/audio.primary.mt6785.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libmedia_helper.so" "libmedia_helper-v30.so" "${2}"
             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;;
-        vendor/lib/hw/audio.usb.mt6785.so)
-            ;&
+        vendor/lib/hw/audio.usb.mt6785.so|\
         vendor/lib64/hw/audio.usb.mt6785.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;;
         vendor/lib64/hw/dfps.mt6785.so)
+  	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             ;;
         vendor/lib64/hw/android.hardware.thermal@2.0-impl.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib64/libwvhidl.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
         vendor/lib64/mediadrm/libwvdrmengine.so)
+	    [ "$2" = "" ] && return 0	
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
-        vendor/bin/mnld)
-            ;&
-        vendor/lib64/libaalservice.so)
-            ;&
+        vendor/bin/mnld|\
+        vendor/lib64/libaalservice.so|\
         vendor/lib64/libcam.utils.sensorprovider.so)
+	    [ "$2" = "" ] && return 0
             grep -q "libshim_sensors.so" "$2" || "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
             ;;
         vendor/lib64/libwifi-hal-mtk.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --set-soname "libwifi-hal-mtk.so" "${2}"
             ;;
         lib64/libem_support_jni.so)
+  	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
             ;;
         vendor/lib/libMtkOmxVdecEx.so|\
         lib/libsource.so)
+	    [ "$2" = "" ] && return 0
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
         vendor/lib/libwvhidl.so)
+	    [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+}
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # Initialize the helper
