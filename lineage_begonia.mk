ecb8220def3cc7e2800807c4d5fc4747bc4d9f73
wodanesdag <wodanesdag@proton.me>
2024-12-23 14:33:04 +0700

    begonia: crDroid Bringup

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -15,8 +15,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common VoltageOS stuff
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
@@ -26,12 +26,9 @@ $(call inherit-product-if-exists, vendor/MiuiCameraLeica/config.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1920
 
-# Official-ify
-VOLTAGE_BUILD_TYPE := UNOFFICIAL
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := voltage_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
